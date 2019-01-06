4456a212007ab11d1eb7c77fa21f9a55f7a1a96b
Coleman Word <coleman@Colemans-MacBook-Pro.local>
2019-01-06 15:38:26 -0700

    CHECKPOINT BETTER ERRORS

--- cmd/info.go
+++ cmd/info.go
@@ -26,14 +26,47 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var (
+	typE string
+)
+func init() {
+	infoCmd.PersistentFlags().StringVarP(&typE, "type", "t", "issues", "type of info needed")
+}
 // infoCmd represents the info command
 var infoCmd = &cobra.Command{
 	Use:   "info",
 	Short: "A brief description of your command",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println(`
-- [ ] Error: Rel: can't make /Users/coleman/go/src/github.com/gofunct/stencil relative to
+		if typE =="issues" {
+			fmt.Println(`
+Colemans-MacBook-Pro:temp coleman$ stencil init -a test
+panic: runtime error: invalid memory address or nil pointer dereference
+[signal SIGSEGV: segmentation violation code=0x1 addr=0x80 pc=0x14806a6]
+
+goroutine 1 [running]:
+github.com/gofunct/stencil/pkg/runtime.(*Runtime).ErrExit(0x0, 0x15a6a55, 0x17, 0x163cae0, 0xc000136ce0)
+        /Users/coleman/go/src/github.com/gofunct/stencil/pkg/runtime/runtime.go:62 +0x26
+github.com/gofunct/stencil/pkg/runtime.(*Runtime).TrimSrcPath(0x0, 0xc000026084, 0x35, 0x0, 0x0, 0x0, 0xc00013ef70)
+        /Users/coleman/go/src/github.com/gofunct/stencil/pkg/runtime/runtime.go:446 +0xba
+github.com/gofunct/stencil/pkg/runtime.NewAppFromPath(0xc000026084, 0x35, 0x0)
+        /Users/coleman/go/src/github.com/gofunct/stencil/pkg/runtime/app.go:84 +0x191
+github.com/gofunct/stencil/cmd.glob..func3(0x19d3a20, 0xc00013caa0, 0x0, 0x2)
+        /Users/coleman/go/src/github.com/gofunct/stencil/cmd/init.go:36 +0x293
+github.com/spf13/cobra.(*Command).execute(0x19d3a20, 0xc00013ca80, 0x2, 0x2, 0x19d3a20, 0xc00013ca80)
+        /Users/coleman/go/pkg/mod/github.com/spf13/cobra@v0.0.3/command.go:766 +0x2cc
+github.com/spf13/cobra.(*Command).ExecuteC(0x19d3c80, 0xc00018df88, 0x10074e0, 0xc000092058)
+        /Users/coleman/go/pkg/mod/github.com/spf13/cobra@v0.0.3/command.go:852 +0x2fd
+github.com/spf13/cobra.(*Command).Execute(0x19d3c80, 0x0, 0x0)
+        /Users/coleman/go/pkg/mod/github.com/spf13/cobra@v0.0.3/command.go:800 +0x2b
+github.com/gofunct/stencil/cmd.Execute()
+        /Users/coleman/go/src/github.com/gofunct/stencil/cmd/root.go:27 +0x2d
+main.main()
+        /Users/coleman/go/src/github.com/gofunct/stencil/main.go:26 +0x20
+
+
+
 `)
+		}
 	},
 }
 
